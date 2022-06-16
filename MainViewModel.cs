6854bc5120db172b19c172756a63b8655017f535
Stephen Miller <steve@mobycorp.com>
2022-06-16 07:42:37 -0700

    Made for the dotnet MAUI bot

---
+++ MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MauiApp1;
+
+public partial class MainViewModel : ObservableObject {
+
+	public MainViewModel () {
+
+		Terms = "By clicking Agree & Join, you agree to the Moby User Agreement and Privacy Policy.";
+	}
+
+	[ObservableProperty]
+	public string terms;
+
+	[ObservableProperty]
+	private string emailAddress;
+
+	[ObservableProperty]
+	private string name;
+
+	[ObservableProperty]
+	private string password;
+
+	[ObservableProperty]
+	private string passwordConfirmation;
+}
